8fcf962f838e07aa5c25662d821b7bd154a3254f
Gabriela Muniz <gabrielanamu@live.com>
2023-06-13 17:31:49 -0300

    feat: add req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação """
+    if '.txt' not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        return "Formato inválido"
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+        return content.split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
